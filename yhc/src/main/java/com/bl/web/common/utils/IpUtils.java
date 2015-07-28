1d9091798c1d705e3f8b068ae4775e055b20ed19
yhc19850706 <yinhuacheng@live.com>
2015-07-28 17:05:35 +0800

    添加 IP地址计算工具类

---
+++ yhc/src/main/java/com/bl/web/common/utils/IpUtils.java
@@ -0,0 +1,57 @@
+package com.bl.web.common.utils;
+
+import org.apache.commons.lang.StringUtils;
+
+public class IpUtils {
+	
+	/**
+     * @param strIp
+     * @return
+     */
+    public static long getIPValue(String strIp) {
+        if (StringUtils.isEmpty(strIp) || "127.0.0.1".equals(strIp)) {
+            return 0;
+        }
+        long[] ip = new long[4];
+        // 先找到IP地址字符串中.的位置
+        int position1 = strIp.indexOf(".");
+        int position2 = strIp.indexOf(".", position1 + 1);
+        int position3 = strIp.indexOf(".", position2 + 1);
+        // 将每个.之间的字符串转换成整型
+        ip[0] = Long.parseLong(strIp.substring(0, position1));
+        ip[1] = Long.parseLong(strIp.substring(position1 + 1, position2));
+        ip[2] = Long.parseLong(strIp.substring(position2 + 1, position3));
+        ip[3] = Long.parseLong(strIp.substring(position3 + 1));
+        return (ip[0] << 24) + (ip[1] << 16) + (ip[2] << 8) + ip[3];
+    }
+
+    /**
+     * @param longIp
+     * @return
+     */
+    public static String longToIP(long longIp) {
+        StringBuffer sb = new StringBuffer("");
+        // 直接右移24位
+        sb.append(String.valueOf((longIp >>> 24)));
+        sb.append(".");
+        // 将高8位置0，然后右移16位
+        sb.append(String.valueOf((longIp & 0x00FFFFFF) >>> 16));
+        sb.append(".");
+        // 将高16位置0，然后右移8位
+        sb.append(String.valueOf((longIp & 0x0000FFFF) >>> 8));
+        sb.append(".");
+        // 将高24位置0
+        sb.append(String.valueOf((longIp & 0x000000FF)));
+        return sb.toString();
+    }
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+//        System.out.println(getIPValue("211.155.87.198"));
+        System.out.println(getIPValue("127.0.0.1"));
+//        System.out.println(longToIP(3550173198L));
+    }
+
+}
