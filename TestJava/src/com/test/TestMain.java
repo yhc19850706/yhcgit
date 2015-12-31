ce65e530f19c2113e6e1479d05b340ca412fd606
yhc19850706 <yinhuacheng@live.com>
2015-12-31 18:35:01 +0800

    提交修改201512311834

--- TestJava/src/com/test/TestMain.java
+++ TestJava/src/com/test/TestMain.java
@@ -1,6 +1,9 @@
 package com.test;
 
+import java.io.UnsupportedEncodingException;
 import java.lang.reflect.Field;
+import java.util.Iterator;
+import java.util.Set;
 
 import javax.xml.crypto.Data;
 
@@ -24,6 +27,30 @@ public class TestMain {
 		System.out.println("str1=str2");
 		else
 		System.out.println("str1<str2");
+		
+		Jedis jedis = new Jedis("127.0.0.1",6379);
+		String keys = "shiro_redis_session:admin";
+		// 取数据 
+				Set<String> value = jedis.keys("*yhc*");
+                 Iterator<String> eIterable=value.iterator();
+                while(eIterable.hasNext()){
+                	String valueString=eIterable.next();
+                	byte[] bytes;
+					try {
+						bytes = valueString.getBytes("utf-8");
+						String str = new String(bytes,"utf-8");
+	                	System.out.println(str);
+					} catch (UnsupportedEncodingException e) {
+						// TODO  Auto-generated catch block
+								e.printStackTrace();
+							
+					}//""里面的参数为需要转化的编码，一般是ISO8859-1 
+                	
+                } 
+		// 删数据
+		// 存数据
+//		jedis.set(keys, "sdfs");
+		// 取数据 
 	    } 
 	   
 }
