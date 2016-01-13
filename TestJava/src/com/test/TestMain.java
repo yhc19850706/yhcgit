465a6f3e5bcb0a20a35b7d4129f843f44af8f0b5
yhc19850706 <yinhuacheng@live.com>
2016-01-13 16:38:08 +0800

    20160113

--- TestJava/src/com/test/TestMain.java
+++ TestJava/src/com/test/TestMain.java
@@ -4,6 +4,9 @@ import java.io.UnsupportedEncodingException;
 import java.lang.reflect.Field;
 import java.util.Iterator;
 import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.xml.crypto.Data;
 
@@ -16,41 +19,34 @@ public class TestMain {
  */
 	
 	public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException { 
-		 String str1="20151212000000",investUserMobile="13813204059";
-		 String str2="20151212";
-		 investUserMobile=investUserMobile.substring(0,3)+"****"+investUserMobile.substring(7,11);
-		 System.out.println("investUserMobile="+investUserMobile);
-		int result=str1.compareTo(str2);
-		if(result>0)
-		System.out.println("str1>str2");
-		else if(result==0)
-		System.out.println("str1=str2");
-		else
-		System.out.println("str1<str2");
-		
-		Jedis jedis = new Jedis("127.0.0.1",6379);
-		String keys = "shiro_redis_session:admin";
-		// 取数据 
-				Set<String> value = jedis.keys("*yhc*");
-                 Iterator<String> eIterable=value.iterator();
-                while(eIterable.hasNext()){
-                	String valueString=eIterable.next();
-                	byte[] bytes;
-					try {
-						bytes = valueString.getBytes("utf-8");
-						String str = new String(bytes,"utf-8");
-	                	System.out.println(str);
-					} catch (UnsupportedEncodingException e) {
-						// TODO  Auto-generated catch block
-								e.printStackTrace();
-							
-					}//""里面的参数为需要转化的编码，一般是ISO8859-1 
-                	
-                } 
-		// 删数据
-		// 存数据
-//		jedis.set(keys, "sdfs");
-		// 取数据 
-	    } 
+//		 String str1="20151212000000",investUserMobile="13813204059";
+//		 String str2="20151212";
+//		 investUserMobile=investUserMobile.substring(0,3)+"****"+investUserMobile.substring(7,11);
+//		 System.out.println("investUserMobile="+investUserMobile);
+//		int result=str1.compareTo(str2);
+//		if(result>0)
+//		System.out.println("str1>str2");
+//		else if(result==0)
+//		System.out.println("str1=str2");
+//		else
+//		System.out.println("str1<str2");
+
+		String str1="com.sillycat.easyaop.dao.impl.RoleDAOImpl.saveRole";
+		  String str2="com.sillycat.easyaop.dao.impl.UserDAOImpl.save";
+		  String str3="com.sillycat.easyaop.dao.impl.BaseDAOImpl.save";
+		  Pattern pattern=Pattern.compile( 
+		    ".*DAOImpl[.]save.*");
+		  Matcher matcher1=pattern.matcher(str1);
+		  Matcher matcher2=pattern.matcher(str2);
+		  Matcher matcher3=pattern.matcher(str3);
+		  System.out.println(matcher1.matches());
+		  System.out.println(matcher2.matches());
+		  System.out.println(matcher3.matches());
+		  
+		 }
+		 static void v(int i)
+		 {
+		  System.out.println("i : "+i);
+		 }
 	   
 }
