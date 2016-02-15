ddae18d4ecf5b0c6086c53e807c7ef888ffaa940
yhc19850706 <yinhuacheng@live.com>
2016-02-15 14:59:11 +0800

    1231

---
+++ TestJava/src/com/concurrent/BlockingQueueUtil.java
@@ -0,0 +1,26 @@
+/**   
+* BlockingQueueUtil.java 
+* com.concurrent 
+* @Description:
+* @author Yhc yinhc@yinhu.com
+* 2016年1月14日 下午2:14:00 
+* @version V1.0   
+*/
+package com.concurrent;
+
+import java.util.concurrent.LinkedBlockingQueue;
+
+		/** 
+ * @Project：TestJava
+ * @Package：com.concurrent
+ * @ClassName: BlockingQueueUtil 
+ * @Description: TODO(这里用一句话描述这个类的作用) 
+ * @author Yhc yinhc@yinhu.com
+ * @date 2016年1月14日 下午2:14:00  
+ */
+public class BlockingQueueUtil {
+
+	LinkedBlockingQueue<String> linkedBlockingQueue=new LinkedBlockingQueue<>();
+}
+
+	
